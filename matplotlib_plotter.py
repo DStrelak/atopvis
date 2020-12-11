d20a196a14ae727345f44c7ec2fb41d847c23c58
David Strelak <davidstrelak@gmail.com>
2020-12-11 10:20:03 +0100

    opening atop on ctrl+click

--- matplotlib_plotter.py
+++ matplotlib_plotter.py
@@ -8,10 +8,11 @@ from atop_report import AtopReport
 class MatplotlibPlotter:
     def __init__(self, report: AtopReport):
         self.report = report
-        self.annotation_texts = {p.time : p for p in report.processes}
+        self.annotation_texts = {p.time: p for p in report.processes}
         self.last_event_xy = ()
         self.last_annotation = None
         self.fig = None
+        self.ctrl_pushed = False
 
     def __add_annotation(self, ax):
         self.last_annotation = ax.annotate("", xy=(0, 0), xytext=(-20, 20),
@@ -48,23 +49,65 @@ class MatplotlibPlotter:
         ax2.legend(lines + lines2, labels + labels2)
         self.__add_annotation(ax2)
 
-    def on_pick(self, event):
+    def __on_key_press(self, event):
+        if 'control' == event.key:
+            print(f'pushing {event.key}')
+            self.ctrl_pushed = True
+
+    def __on_key_release(self, event):
+        if 'control' == event.key:
+            self.ctrl_pushed = False
+
+    def __show_process_annotation(self, time):
+        text = str(self.annotation_texts[time])
+        text = text + '\nPress Ctrl+Click to open atop'
+        self.last_annotation.set_text(text)
+        self.last_annotation.set_visible(True)
+        self.fig.canvas.draw_idle()
+
+    def __open_atop(self, time):
+        import distutils.spawn
+        import os.path
+        import subprocess
+        atop = distutils.spawn.find_executable('atop')
+        terminal = distutils.spawn.find_executable('gnome-terminal')
+        text = ''
+        if not atop or not terminal:
+            text = 'atop or gnome-terminal not found'
+        file = self.report.file
+        if not os.path.exists(file):
+            text = f'file {file} does not exist'
+        if text:
+            self.last_annotation.set_text(text)
+            self.last_annotation.set_visible(True)
+            self.fig.canvas.draw_idle()
+        else:
+            self.ctrl_pushed = False  # otherwise if user releases Ctrl in terminal, we won't get the event
+            subprocess.call(f'gnome-terminal --maximize -- atop -b {time} -r {file}', shell=True)
+
+    def __on_pick(self, event):
         xy = (event.mouseevent.x, event.mouseevent.y)
         if xy == self.last_event_xy:
             return  # ignore multiple events at the same location
         self.last_event_xy = xy
         xydata = (event.mouseevent.xdata, event.mouseevent.ydata)
         time = mdates.num2date(xydata[0]).strftime('%H:%M:%S')
-        text = str(self.annotation_texts[time])
         self.last_annotation.xy = xy
-        self.last_annotation.set_text(text)
-        self.last_annotation.set_visible(True)
-        self.fig.canvas.draw_idle()
+        if self.ctrl_pushed:
+            self.__open_atop(time)
+        else:
+            self.__show_process_annotation(time)
 
-    def on_axes_leave(self, event):
+    def __on_figure_leave(self, event):
         if self.last_annotation is not None and self.last_annotation.get_visible():
             self.last_annotation.set_visible(False)
             self.fig.canvas.draw_idle()
+
+    def __register_events(self):
+        self.fig.canvas.mpl_connect('pick_event', self.__on_pick)
+        self.fig.canvas.mpl_connect('figure_leave_event', self.__on_figure_leave)
+        self.fig.canvas.mpl_connect('key_press_event', self.__on_key_press)
+        self.fig.canvas.mpl_connect('key_release_event', self.__on_key_release)
 
     def plot(self, interactive, destination):
         resources = self.report.resources
@@ -79,9 +122,7 @@ class MatplotlibPlotter:
                 self.__set2(ax, r.data_opt_unit, r.data_opt)
 
         self.fig.set_size_inches(20, len(resources) * 2.5)
-        self.fig.set_picker(True)
-        self.fig.canvas.mpl_connect('pick_event', self.on_pick)
-        self.fig.canvas.mpl_connect('axes_leave_event', self.on_axes_leave)
+        self.__register_events()
         if destination:
             plt.savefig(destination, dpi=300, bbox_inches='tight')
         if interactive:
