c7718179fc0e217c0a7e726e85476ecdbc1134c1
David Strelak <davidstrelak@gmail.com>
2020-12-17 19:57:59 +0100

    first version of the per-process analysis

--- atop_constants.py
+++ atop_constants.py
@@ -1,1 +1,39 @@
+import sys
 ATOP_TIMESTAMP = 'timestamp'
+
+# since Python 3.6, dicts keep insertion order
+assert sys.version_info >= (3, 6)
+
+# copied (and slightly altered) from man
+COMMON_FIELDS = {'label': str, 'host': str, 'epoch': int, 'date': str, 'time': str, 'interval': str}
+SEP = 'SEP'
+RESET = 'RESET'
+PRG_FIELDS = {**COMMON_FIELDS, **{'pid': int, 'name': str, 'state': str, 'uid-real': int, 'gid-real': int, 'tgid': int,
+                                  'threads-count': int, 'exit-code': int, 'start': int, 'command': str, 'ppid': int,
+                                  'threads-r': int, 'threads-s': int, 'threads-d': int, 'uid-effective': int,
+                                  'gid-effective': int, 'uid-saved': int, 'gid-saved': int, 'uid-filesystem': int,
+                                  'gid-filesystem': int, 'elapsed-time': int, 'is-process': str, 'vpid': int,
+                                  'ctid': int, 'cid': int}}
+PRG_FIELDS_BETWEEN_BRACKETS = ['name', 'command']
+
+PRC_FIELDS = {**COMMON_FIELDS, **{'pid': int, 'name': str, 'state': str, 'clock-ticks': int, 'cpu-usr': int,
+                                  'cpu-sys': int, 'nice': int, 'priority': int, 'priority-realtime': int,
+                                  'scheduling': int, 'cpu-cur': int, 'sleep-avg': int, 'tgid': int, 'is-process': str}}
+PRC_FIELDS_BETWEEN_BRACKETS = ['name']
+
+PRM_FIELDS = {**COMMON_FIELDS, **{'pid': int, 'name': str, 'state': str, 'page-bytes': int, 'mem-virt-kbytes': int,
+                                  'mem-res-kbytes': int, 'mem-shared-text-kbytes': int, 'mem-virt-growth-kbytes': int,
+                                  'mem-res-growth-kbytes': int, 'page-faults-minor': int, 'page-faults-major': int,
+                                  'exec-size-kbytes': int, 'data-size-kbytes': int, 'stack-size-kbytes': int,
+                                  'swap-kbytes': int, 'tgid': int, 'is-process': str, 'set-size-kbytes': int}}
+PRM_FIELDS_BETWEEN_BRACKETS = ['name']
+
+PRE_FIELDS = {**COMMON_FIELDS, **{'pid': int, 'name': str, 'state': str, 'gpu-state': str, 'gpus-used': int,
+                                  'bitlist': int, 'busy': int, 'mem-busy': int, 'mem-util-curr-kb': int,
+                                  'mem-util-kb': int, 'samples': int}}
+PRE_FIELDS_BETWEEN_BRACKETS = ['name']
+
+PRD_FIELDS = {**COMMON_FIELDS, **{'pid': int, 'name': str, 'state': str, 'obsolete-kernel': str, 'std-stat-used': str,
+                                  'reads': int, 'read-sectors': int, 'writes': int, 'write-sectors': int,
+                                  'write-cancelled': int, 'tgid': int, 'is-process': str}}
+PRD_FIELDS_BETWEEN_BRACKETS = ['name']
---
+++ process_info.py
@@ -0,0 +1,164 @@
+import subprocess
+import pandas as pd
+import logging
+import re
+from atop_constants import *
+
+LOGGER = logging.getLogger()
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+def __run(cmd):
+    LOGGER.debug('__run called')
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    log = []
+    while True:
+        output = p.stdout.readline().decode("utf-8").rstrip('\n')
+        if output == '' and p.poll() is not None:
+            break
+        if '' == output:
+            continue
+        log.append(output)
+    return 0 == p.poll(), log
+
+
+class ProcessInfo:
+    def __init__(self, pid, name, command, start):
+        self.pid = pid
+        self.name = name
+        self.command = command
+        self.start = start  # epoch
+        self.end = None  # epoch
+        self.records = {}
+
+    def update(self, time, data: dict):
+        if time not in self.records:
+            self.records[time] = data
+        else:
+            self.records[time].update(data)
+
+    def set_end(self, end):
+        self.end = end
+
+    def __repr__(self):
+        return str(vars(self))
+
+
+def get_token(t, tokens, fields, with_brackets):
+    index = list(fields.keys()).index(t)
+    val = tokens[index]
+    if t in with_brackets:
+        val = val.replace('(', '').replace(')', '')
+    return fields[t](val)
+
+
+def parse_general(file, label):
+    success, log = __run(f'atop -r {file} -P {label}')
+    if not success:
+        LOGGER.critical(f'Could not obtain process data for file {file} and label {label}')
+        exit(-1)
+    # split on space, except when it's between brackets
+    pattern = re.compile(r'\s+(?=[^()]*(?:\(|$))')
+    #pattern = re.compile(r'\s+|(\(.*?\))')
+    for line in log:
+        if line.startswith(SEP) or line.startswith(RESET):
+            continue
+        tokens = pattern.split(line)
+        yield tokens
+
+
+def parse_prg(file):
+    def get(t):
+        return get_token(t, tokens, PRG_FIELDS, PRG_FIELDS_BETWEEN_BRACKETS)
+    processes = {}
+    for tokens in parse_general(file, 'PRG'):
+        pid = get('pid')
+        process = processes.setdefault(pid, ProcessInfo(pid, get('name'), get('command'), get('start')))
+        state = get('state')
+        if 'E' in state:
+            process.set_end(get('epoch'))
+    return processes
+
+
+def update_general(file, processes, label, fields, all_fields, fields_between_brackets):
+    def get(t):
+        return get_token(t, tokens, all_fields, fields_between_brackets)
+
+    def kv(k):
+        return k, get(k)
+    for tokens in parse_general(file, label):
+        process = processes[get('pid')]
+        process.update(get('epoch'), dict(map(kv, fields)))
+
+
+def update_prc(file, processes):
+    LOGGER.debug('update prc started')
+    update_general(file, processes, 'PRC', ['clock-ticks', 'cpu-usr', 'cpu-sys'],
+                   PRC_FIELDS, PRC_FIELDS_BETWEEN_BRACKETS)
+    LOGGER.debug('update prc done')
+
+
+def update_prm(file, processes):
+    LOGGER.debug('update prm done')
+    update_general(file, processes, 'PRM', ['data-size-kbytes', 'swap-kbytes'],
+                   PRM_FIELDS, PRM_FIELDS_BETWEEN_BRACKETS)
+    LOGGER.debug('update prm done')
+
+
+def update_pre(file, processes):
+    update_general(file, processes, 'PRE', ['busy', 'mem-busy', 'mem-util-kb'],
+                   PRE_FIELDS, PRE_FIELDS_BETWEEN_BRACKETS)
+
+
+def update_prd(file, processes):
+    update_general(file, processes, 'PRD', ['read-sectors', 'write-sectors'],
+                   PRD_FIELDS, PRD_FIELDS_BETWEEN_BRACKETS)
+
+
+def get_statistics(processes):
+    for k, v in processes.items():
+        LOGGER.debug(f'Computing statistics for pid {k}')
+        data = v.records
+        if data:  # skip empty data
+            df = pd.DataFrame.from_dict(data, orient='index')
+            for field, records in df.describe().items():
+                for metric, value in records.items():
+                    setattr(v, f'{field}_{metric}', value)
+
+    def to_dict(r):
+        d = vars(r)
+        d.pop('records')
+        return d
+    LOGGER.debug(f'Converting to excel')
+    df = pd.DataFrame.from_dict([to_dict(p) for p in processes.values()])
+    df.to_excel('test.xls')
+
+
+def main(args):
+    file = args.atop
+    processes = parse_prg(file)
+    update_prc(file, processes)
+    update_prm(file, processes)
+    update_pre(file, processes)
+    update_prd(file, processes)
+    get_statistics(processes)
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument('-atop', help='path to the atop file')
+    input_group.add_argument('-pickle', help='path to pickle file')
+
+    parser.add_argument('-to_png', help='path to the png file')
+    parser.add_argument('-to_pickle', help='path to pickle')
+    parser.add_argument('-i', '--interactive', help='open interactive plot', action='store_true')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    import cProfile
+    #cProfile.run('
+    main(parse_args())
